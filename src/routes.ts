26aa83b743322cab88143e30378345cef59d0f85
Michael Knopke <knopkem@gmail.com>
2021-12-25 10:28:19 +0100

    merging duplicate results, rather than concatenating

--- src/routes.ts
+++ src/routes.ts
@@ -7,12 +7,27 @@ import { LoggerSingleton } from "./utils/logger";
 
 import deepmerge from "deepmerge";
 
+const combineMerge = (target: any, source: any, options: any) => {
+    const destination = target.slice()
+ 
+    source.forEach((item: any, index: number) => {
+        if (typeof destination[index] === 'undefined') {
+            destination[index] = options.cloneUnlessOtherwiseSpecified(item, options)
+        } else if (options.isMergeableObject(item)) {
+            destination[index] = deepmerge(target[index], item, options)
+        } else if (target.indexOf(item) === -1) {
+            destination.push(item)
+        }
+    })
+    return destination
+}
+const options = { arrayMerge: combineMerge };
 const logger = LoggerSingleton.Instance;
 module.exports = function(server: any, opts: any, done: any) {
 
     server.get('/rs/studies', async (req: any, reply: any) => {
         try {
-            const json = deepmerge.all(await doFind(QUERY_LEVEL.STUDY, req.query));
+            const json = deepmerge.all(await doFind(QUERY_LEVEL.STUDY, req.query), options);
             reply.send(json);
         } catch (error) {
             logger.error(error);
@@ -28,7 +43,7 @@ module.exports = function(server: any, opts: any, done: any) {
         query.StudyInstanceUID = params.studyInstanceUid;
 
         try {
-            const json = deepmerge.all(await doFind(QUERY_LEVEL.SERIES, query));
+            const json = deepmerge.all(await doFind(QUERY_LEVEL.SERIES, query), options);
             reply.send(json);
         } catch (error) {
             logger.error(error);
@@ -44,7 +59,7 @@ module.exports = function(server: any, opts: any, done: any) {
         query.StudyInstanceUID = params.studyInstanceUid;
 
         try {
-            const json = deepmerge.all(await doFind(QUERY_LEVEL.SERIES, query));
+            const json = deepmerge.all(await doFind(QUERY_LEVEL.SERIES, query), options);
             reply.send(json);
         } catch (error) {
             logger.error(error);
@@ -61,7 +76,7 @@ module.exports = function(server: any, opts: any, done: any) {
         query.SeriesInstanceUID = params.seriesInstanceUid;
 
         try {
-            const json = deepmerge.all(await doFind(QUERY_LEVEL.IMAGE, query));
+            const json = deepmerge.all(await doFind(QUERY_LEVEL.IMAGE, query), options);
             reply.send(json);
         } catch (error) {
             logger.error(error);
