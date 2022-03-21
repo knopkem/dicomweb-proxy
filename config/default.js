1510b0b491868cb78739dc69de9996215b1b57d8
Pablo Hinojosa <pablohn6@gmail.com>
2022-03-22 00:09:25 +0100

    Update default.js with two more options (Fix #73)
    #73

--- config/default.js
+++ config/default.js
@@ -71,4 +71,10 @@ config.websocketUrl = '';
 config.websocketToken =
   'cf83e1357eefb8bdf1542850d66d8007d620e4050b5715dc83f4a921d36ce9ce47d0d13c5d85f2b0ff8318d2877eec2f63b931bd47417a81a538327af927da3e';
 
+// to enable Keycloack authentication you need a keycloack.json file on root directory
+config.useKeycloakAuth = false;
+
+// clear cache on startup 
+config.clearCacheOnStartup = true;
+
 module.exports = config;
