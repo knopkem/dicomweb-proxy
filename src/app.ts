025d23879ea3de7ca6d01cc330e3a86f3200fb54
Michael Knopke <knopkem@gmail.com>
2022-07-17 12:27:23 +0200

    minor update

--- src/app.ts
+++ src/app.ts
@@ -6,10 +6,8 @@ import fastifySensible from '@fastify/sensible';
 import fastifyHelmet from '@fastify/helmet';
 import fastifyAutoload from '@fastify/autoload';
 
-import { promises } from 'fs';
 import { ConfParams, config } from './utils/config';
-import { sendEcho } from './dimse/sendEcho';
-import { startScp, shutdown } from './dimse/store';
+import { shutdown } from './dimse/store';
 import { LoggerSingleton } from './utils/logger';
 import { socket } from './socket';
 
@@ -33,45 +31,6 @@ server.register(fastifyAutoload, {
   dir: path.join(__dirname, 'routes'),
   options: { prefix: '/viewer' },
 });
-
-//------------------------------------------------------------------
-
-const getDirectories = async (source: string) => {
-  try {
-    const dir = await promises.readdir(source, { withFileTypes: true })
-    return dir.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
-  }
-  catch (e) {
-    logger.warn("Storage Folder doesn't exist: ", source);
-    return [];
-  }
-}
-
-//------------------------------------------------------------------
-
-const clearCache = async () => {
-  const storagePath = config.get(ConfParams.STORAGE_PATH) as string;
-  const retention = config.get(ConfParams.CACHE_RETENTION) as number;
-
-  if (retention < 0) {
-    logger.warn('cache cleanup disabled');
-    return;
-  }
-
-  const dirs = await getDirectories(storagePath);
-  const dateNow = new Date();
-
-  for (const dir of dirs) {
-    const filepath = path.join(storagePath, dir);
-    const stats = await promises.stat(filepath);
-    const mtime = stats.mtime;
-    const minutes = (dateNow.getTime() - mtime.getTime()) / 60000;
-    if (minutes > retention) {
-      logger.info(`removing: ${filepath}`);
-      await promises.rm(filepath, { recursive: true, force: true });
-    }
-  }
-};
 
 //------------------------------------------------------------------
 
@@ -109,22 +68,6 @@ server.listen({ port, host: '0.0.0.0' }, async (err, address) => {
     process.exit(1);
   }
   logger.info(`web-server listening on port: ${port}`);
-
-  // if not using c-get, start our scp
-  if (!config.get(ConfParams.C_GET)) {
-    startScp();
-  }
-  sendEcho();
-
-  const websocketUrl = config.get(ConfParams.WEBSOCKET_URL);
-  if (websocketUrl) {
-    logger.info(`connecting to dicomweb.websocket-bridge: ${websocketUrl}`);
-    socket.connect();
-  }
-
-  // running clear cache and setup for 1h checks
-  clearCache();
-  setInterval(clearCache, 60000);
 });
 
 //------------------------------------------------------------------
--- src/utils/fileHelper.ts
+++ src/utils/fileHelper.ts
@@ -1,4 +1,21 @@
 import fs from 'fs';
+import path from 'path';
+import { promises } from 'fs';
+import { ConfParams, config } from './config';
+import { LoggerSingleton } from './logger';
+
+
+const getDirectories = async (source: string) => {
+  try {
+    const dir = await promises.readdir(source, { withFileTypes: true })
+    return dir.filter((dirent) => dirent.isDirectory()).map((dirent) => dirent.name)
+  }
+  catch (e) {
+    const logger = LoggerSingleton.Instance;
+    logger.warn("Storage Folder doesn't exist: ", source);
+    return [];
+  }
+}
 
 export async function fileExists(pathname: string): Promise<boolean> {
   return new Promise((resolve) => {
@@ -7,3 +24,28 @@ export async function fileExists(pathname: string): Promise<boolean> {
     });
   });
 }
+
+export async function clearCache() {
+  const storagePath = config.get(ConfParams.STORAGE_PATH) as string;
+  const retention = config.get(ConfParams.CACHE_RETENTION) as number;
+  const logger = LoggerSingleton.Instance;
+
+  if (retention < 0) {
+    logger.warn('cache cleanup disabled');
+    return;
+  }
+
+  const dirs = await getDirectories(storagePath);
+  const dateNow = new Date();
+
+  for (const dir of dirs) {
+    const filepath = path.join(storagePath, dir);
+    const stats = await promises.stat(filepath);
+    const mtime = stats.mtime;
+    const minutes = (dateNow.getTime() - mtime.getTime()) / 60000;
+    if (minutes > retention) {
+      logger.info(`removing: ${filepath}`);
+      await promises.rm(filepath, { recursive: true, force: true });
+    }
+  }
+};
